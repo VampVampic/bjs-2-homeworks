8ad19149c24063131de670340f94dc42461bd940
VampVampich <vam-pzum@mail.ru>
2024-10-12 16:04:01 +0400

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(meaning) {          
+    if (Number.isNaN(Number.parseFloat(meaning))) {
+        throw new Error('Невалидное значение');
+    }
+    return parseFloat(meaning);
+}
+
+function validateCount(meaning) {
+    try {
+        return parseCount(meaning);
+    } catch (error) {
+        return error;
+    }
+} 
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error('Треугольник с такими сторонами не существует')
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
